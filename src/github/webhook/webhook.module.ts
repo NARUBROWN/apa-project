00669a37a2f75f30ae76b0c25304d50efd80a749
NARUBROWN <codingshark@inu.ac.kr>
2025-09-13 17:57:10 +0900

    feat: API 클라이언트 서비스 분리
    Webhook Service에서 Octokit 인스턴스를 직접 생성하고 Github API를 호출하는 로직을 별도의 GithubApiService로 분리하여 역할과 책임을 명확히 나눴다

--- src/github/webhook/webhook.module.ts
+++ src/github/webhook/webhook.module.ts
@@ -3,10 +3,11 @@ import { WebhookController } from './webhook.controller.js';
 import { WebhookService } from './webhook.service.js';
 import { OpenaiService } from '../../ai/openai/openai.service.js';
 import { ConfigModule } from '@nestjs/config';
+import { GithubApiService } from '../github-api/github-api.service.js';
 
 @Module({
   imports: [ConfigModule], 
   controllers: [WebhookController],
-  providers: [WebhookService, OpenaiService],
+  providers: [WebhookService, OpenaiService, GithubApiService],
 })
 export class WebhookModule {}
